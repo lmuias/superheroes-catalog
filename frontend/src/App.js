10ce5a5f7cad120dc0fd940cb47e06fe082e59ab
Valentyn Sobol <valiksobol14@gmail.com>
2024-11-08 22:04:08 +0200

    change import to use

--- frontend/src/App.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { Outlet } from 'react-router-dom';
-import style from './App.module.scss';
-export const App = () => {
-    return (_jsx("main", { className: style.main, children: _jsx(Outlet, {}) }));
-};
--- frontend/src/components/LoaderButton/LoaderButton.js
+++
@@ -1,3 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import style from './LoaderButton.module.scss';
-export const LoaderButton = () => (_jsx("button", { type: 'button', className: style.button, "data-cy": "Loader", children: _jsx("div", { className: style.content }) }));
