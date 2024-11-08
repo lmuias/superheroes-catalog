23c235ce2611f996006eecbc9129e6fa2bdbb178
Valentyn Sobol <valiksobol14@gmail.com>
2024-11-08 22:46:56 +0200

    another try to fix corse

--- backend/app.js
+++ backend/app.js
@@ -15,13 +15,7 @@ const allowedOrigins = ['https://superheroes-catalog-2.onrender.com'];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   })
