59cff23f07d7e256d22f0b49b353169f87801a65
Valentyn Sobol <valiksobol14@gmail.com>
2024-11-08 15:39:31 +0200

    add env file

--- backend/services/db.js
+++ backend/services/db.js
@@ -1,11 +1,14 @@
+import 'dotenv/config';
 import pkg from 'pg';
 const { Client } = pkg;
 
+console.log('DB Password:', process.env.DB_PASSWORD);
+
 export const client = new Client({
-  host: 'localhost',
-  user: 'postgres',
-  password: 'root',
-  database: 'postgres',
+  host: process.env.DB_HOST,
+  user:  process.env.DB_USER,
+  password:  process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 await client.connect();
