2a32b82f5fe367ea10167956464992605c8bece1
Valentyn Sobol <valiksobol14@gmail.com>
2024-11-08 22:17:21 +0200

    change mixins and variables import

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "src/styles/common.scss";`
+        additionalData: `@use "src/styles/common.scss";`
       },
     },
   },
