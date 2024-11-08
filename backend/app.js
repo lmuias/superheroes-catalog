4205dcca3c22c084fd5d1891c9f8360bf84fba8f
Valentyn Sobol <valiksobol14@gmail.com>
2024-11-08 22:40:08 +0200

    try to fix cors problem

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,21 @@ const port = process.env.PORT || 8080;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(cors());
+const allowedOrigins = ['https://superheroes-catalog-2.onrender.com'];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 
 app.use('/heroes', cors(), express.json(), heroRouter);
 
